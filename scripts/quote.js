bcbaa4ace073e106ec194894a9d1dec2f5406247
Esben Aarseth <Esben.Aarseth@bekk.no>
2016-11-12 11:46:34 +0100

    Use whatdoestrumpthink.com to get quotes.

--- scripts/quote.js
+++ scripts/quote.js
@@ -2,14 +2,35 @@
 //  I am very, very, smart. I have one of the highest IQs. This is my proof.
 //
 // Commands:
-//   hubot quote - I will give you a quote
+//   hubot quote random - I will give you a random quote
+//   hubot quote <name> - I will give you a quote concerning <Name>
 //   hubot cyber - I will teach you cyber. My son is very very good at it.
-var quotes = require('./lib/quotes');
 var cyber = require('./lib/cyber');
 
 module.exports =  function(robot) {
-    robot.respond(/quote/, function(res) {
-        res.send(res.random(quotes));
+    robot.respond(/quote\s(.+)/, function(res) {
+        var name = res.match[1];
+        robot.http('https://api.whatdoestrumpthink.com/api/v1/quotes/personalized?q=' + name)
+            .header('Accept', 'application/json')
+            .get()(function(err, response, body) {
+            if (err) {
+                res.send('I do not make mistakes. I think Crooked Hillary have intercepted the request on her email-server. She should be put in prison.');
+            }
+            var quote = JSON.parse(body);
+            res.send(quote.message);
+        });
+    });
+
+    robot.respond(/quote\srandom/, function(res) {
+        robot.http('https://api.whatdoestrumpthink.com/api/v1/quotes/random')
+            .header('Accept', 'application/json')
+            .get()(function(err, response, body) {
+            if (err) {
+                res.send('I do not make mistakes. I think Crooked Hillary have intercepted the request on her email-server. She should be put in prison.');
+            }
+            var quote = JSON.parse(body);
+            res.send(quote.message);
+        });
     });
 
     robot.respond(/cyber/, function(res) {
